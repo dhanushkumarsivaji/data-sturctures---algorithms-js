8cec82d2bae77f624e622ed776bdb22c7f2347cb
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2023-02-09 19:26:17 +0530

    Completed Binary Search Trees

---
+++ binary-search-trees/insert/script.js
@@ -0,0 +1,45 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null
+    }
+
+    insert(value){
+        const newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode
+            return this
+        }
+        let temp = this.root
+        while(true){
+            if(newNode.value === temp.value) return undefined
+            if(newNode.value < temp.value){
+                if(temp.left === null){
+                    temp.left = newNode
+                    return this
+                }
+                temp = temp.left
+            }else{
+                if(temp.right === null){
+                    temp.right = newNode
+                    return this
+                }
+                temp = temp.right
+            }   
+        }
+    }
+}
+
+let myBST = new BST()
+myBST.insert(47)
+myBST.insert(21)
+myBST.insert(18)
+myBST.insert(16)
+myBST.insert(12)
---
+++ binary-search-trees/minValueNode/script.js
@@ -0,0 +1,70 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null
+    }
+
+    insert(value){
+        const newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode
+            return this
+        }
+        let temp = this.root
+        while(true){
+            if(newNode.value === temp.value) return undefined
+            if(newNode.value < temp.value){
+                if(temp.left === null){
+                    temp.left = newNode
+                    return this
+                }
+                temp = temp.left
+            }else{
+                if(temp.right === null){
+                    temp.right = newNode
+                    return this
+                }
+                temp = temp.right
+            }   
+        }
+    }
+
+    contains(value){
+        if(this.root === null) return false
+        let temp = this.root
+        while(temp){
+            if(value < temp.value){
+                temp = temp.left
+            }else if (value > temp.value){
+                temp = temp.right
+            }else{
+                return true
+            }
+        }
+        return false
+    }
+
+    minValueNode(currentNode){
+        while(currentNode.left !==null){
+            currentNode = currentNode.left
+        }
+        return currentNode
+    }
+}
+
+let myBST = new BST()
+myBST.insert(47)
+myBST.insert(21)
+myBST.insert(18)
+myBST.insert(16)
+myBST.insert(12)
+myBST.minValueNode(myBST.root)
+myBST.minValueNode(myBST.root.right)
+
