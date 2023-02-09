aa9f1ebf00481a307b6d258d95295373df811107
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2023-02-09 10:57:15 +0530

    Completed Stacks

---
+++ stack/pop/script.js
@@ -0,0 +1,41 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor(value){
+        const newNode = new Node(value)
+        this.top = newNode
+        this.length = 1
+    }
+
+    push(value){
+        const newNode = new Node(value)
+        if(this.length === 0){
+            this.top = newNode
+        }else{
+            newNode.next = this.top
+            this.top = newNode
+        }
+        this.length++
+        return this
+    }
+
+    pop(){
+        if(this.length===0) return undefined
+        let temp = this.top
+        this.top = this.top.next
+        temp.next = null
+        this.length--
+        return temp
+    }
+}
+
+let myStack = new Stack(11)
+myStack.push(12)
+myStack.push(13)
+myStack.push(14)
+myStack.pop()
---
+++ stack/push/script.js
@@ -0,0 +1,29 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor(value){
+        const newNode = new Node(value)
+        this.top = newNode
+        this.length = 1
+    }
+
+    push(value){
+        const newNode = new Node(value)
+        if(this.length === 0){
+            this.top = newNode
+        }else{
+            newNode.next = this.top
+            this.top = newNode
+        }
+        this.length++
+        return this
+    }
+}
+
+let myStack = new Stack(11)
+myStack.push(12)
