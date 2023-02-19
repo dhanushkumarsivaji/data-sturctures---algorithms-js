050e1d941dcf2eeeb612e144d065aa40e2d671d9
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2023-02-19 22:32:52 +0530

    Completed Tree Traversal

---
+++ tree-traversal/depth-first-search/post-order/script.js
@@ -0,0 +1,105 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null
+    }
+
+    insert(value){
+        const newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode
+            return this
+        }
+        let temp = this.root
+        while(true){
+            if(newNode.value === temp.value) return undefined
+            if(newNode.value < temp.value){
+                if(temp.left === null){
+                    temp.left = newNode
+                    return this
+                }
+                temp = temp.left
+            }else{
+                if(temp.right === null){
+                    temp.right = newNode
+                    return this
+                }
+                temp = temp.right
+            }   
+        }
+    }
+
+    contains(value){
+        if(this.root === null) return false
+        let temp = this.root
+        while(temp){
+            if(value < temp.value){
+                temp = temp.left
+            }else if (value > temp.value){
+                temp = temp.right
+            }else{
+                return true
+            }
+        }
+        return false
+    }
+
+    minValueNode(currentNode){
+        while(currentNode.left !==null){
+            currentNode = currentNode.left
+        }
+        return currentNode
+    }
+
+    BFS(){
+        let currentNode = this.root
+        let results = []
+        let queue = []
+        queue.push(currentNode)
+
+        while(queue.length){
+            currentNode = queue.shift()
+            results.push(currentNode.value)
+            if(currentNode.left) queue.push(currentNode.left)
+            if(currentNode.right) queue.push(currentNode.right)
+        }
+        return results
+    }
+
+    DFSPreOrder(){
+        let results = []
+        function traverse(currentNode){
+            results.push(currentNode.value)
+            if(currentNode.left) traverse(currentNode.left)
+            if(currentNode.right) traverse(currentNode.right)
+        }
+        traverse(this.root)
+        return results
+    }
+
+    DFSPostOrder(){
+        let results = []
+        function traverse(currentNode){
+            if(currentNode.left) traverse(currentNode.left)
+            if(currentNode.right) traverse(currentNode.right)
+            results.push(currentNode.value)
+        }
+        traverse(this.root)
+        return results
+    }
+}
+
+let myBST = new BST()
+myBST.insert(47)
+myBST.insert(21)
+myBST.insert(18)
+myBST.insert(16)
+myBST.insert(12)
+
